c0a21acf842483bf648ec70eac8c8120bb744907
ittetsu <ittetsu.1118@icloud.com>
2023-09-08 23:07:17 +0900

    ubuntuで開いた時の文字化けを直した

--- ColorQuiz/Assets/GameSetting.cs
+++ ColorQuiz/Assets/GameSetting.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameSetting : MonoBehaviour
 {
-    // test_set �� color_set �͊e�v�f����v����悤�ɒ�`����
+    // test_set と color_set は一致させること
     public static string[] text_set = new string[4] { "red", "blue", "green", "yellow" };
     public static Color[] color_set = new Color[4] { Color.red, Color.blue, Color.green, Color.yellow };
 
